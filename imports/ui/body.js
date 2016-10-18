5b65d0b1c838833e2d5e16f9d50d0b4592bfbc15
atefBB <atef.bettaib@gmail.com>
2016-10-18 21:55:17 +0100

    re-edit imports/ui/body.js file

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,10 +9,19 @@ import './body.html';
 Template.body.onCreated(function bodyOnCreated() {
   this.state = new ReactiveDict();
 });
- 
+
 Template.body.helpers({
   tasks() {
-    // Show newest tasks at the top
+  	const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) 
+    {
+      // If hide completed is checked, filter tasks
+      return Tasks.find(
+	      				{ checked: { $ne: true } },
+	      				{ sort: { createdAt: -1 } }
+	      				);
+    }
+    // Otherwise, return all of the tasks
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
 });
@@ -34,5 +43,8 @@ Template.body.events({
  
     // Clear form
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   },
 });
